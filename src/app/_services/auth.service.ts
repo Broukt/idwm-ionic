e328491ae04a3c87c8474d9bbb940c593da5df60
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2024-06-15 17:22:30 -0400

    feat: add auth service and login function

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { environment } from 'src/environments/envirorment.development';
+import { Auth } from '../_interfaces/auth';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  baseUrl: string = environment.apiUrl;
+  private currentAuthSource = new BehaviorSubject<Auth | null>(null);
+  currentAuth$ = this.currentAuthSource.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  login(model: any) {
+    return this.http.post<Auth>(`${this.baseUrl}/auth/login`, model).pipe(
+      map((auth: Auth) => {
+        if (auth) {
+          this.setCurrentAuth(auth);
+        }
+      })
+    );
+  }
+
+  setCurrentAuth(auth: Auth) {
+    localStorage.setItem('auth', JSON.stringify(auth));
+    this.currentAuthSource.next(auth);
+  }
+}
