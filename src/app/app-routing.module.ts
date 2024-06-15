bbc39ed1a94ba471ffe2493f3beb364ad9b0fa1a
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2024-06-15 08:32:22 -0400

    feat: add redirect to auth page when opening the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./_pages/auth/auth.module').then( m => m.AuthPageModule)
+  },
+  {
+    path: 'receipt',
+    loadChildren: () => import('./_pages/receipt/receipt.module').then( m => m.ReceiptPageModule)
   }
+
 ];
 @NgModule({
   imports: [
