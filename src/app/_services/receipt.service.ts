7b1d2b9946039d91f2312750fa5d8b613a60dba9
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2024-06-26 08:15:24 -0400

    feat: add receipt service

--- src/app/_services/receipt.service.ts
+++ src/app/_services/receipt.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { environment } from 'src/environments/envirorment.development';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Purchase } from '../_interfaces/purchase';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReceiptService {
+  baseUrl: string = environment.apiUrl;
+  private currentPurchaseSource = new BehaviorSubject<Purchase[] | null>(null);
+  currentPurchase$ = this.currentPurchaseSource.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  getPurchases() {
+    let sub = this.authService.currentAuth$.subscribe((auth) => {
+      if (!auth) {
+        console.log('No hay auth');
+        //go back to previous screen
+        return;
+      }
+      let headers = new HttpHeaders().set(
+        'Authorization',
+        `Bearer ${auth.token}`
+      );
+      let id = auth.user.id;
+
+      this.http
+        .get<Purchase[]>(`${this.baseUrl}/user/${id}/purchases`, { headers })
+        .subscribe((purchases) => {
+          console.log('purchases');
+          console.log(purchases);
+          if (purchases) {
+            this.currentPurchaseSource.next(purchases);
+          }
+        });
+    });
+
+    sub.unsubscribe();
+  }
 }
